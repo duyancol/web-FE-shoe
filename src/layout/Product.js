4f2c121e1045fcea8aa7d92a34b2c4b89e983c2a
nguyenduy.30719@gmail.com <nguyenduy.30719@gmail.com>
2024-09-06 11:31:28 +0700

    up resize

--- src/layout/Product.js
+++ src/layout/Product.js
@@ -485,7 +485,7 @@ React.useEffect(
                         {row.quantity>0 ? ( 
                            <></>
                          
-                             ):<div className="product-grid1"> <img src={"https://shop-shoe-1-heb5.onrender.com/images/img/sold-removebg-preview (1).png"} ></img></div>}
+                             ):<div className="product-grid1"> <img src={"https://raw.githubusercontent.com/duyancol/web-FE-shoe/main/src/layout/images/sold-removebg-preview%20(1).png"} ></img></div>}
                        
                         <Link className='btn ' to={`/products/${row.id}`}><img src='../images/eye.png'></img></Link>
                         <div className="product-grid-head">
